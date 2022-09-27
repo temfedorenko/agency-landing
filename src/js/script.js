81fdefefb616ddf2d9b3fa69ad04278d9345cfe5
ArtFED <temfedorenko@gmail.com>
2022-09-27 18:34:53 +0300

    portfolio-all

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+// 'use strict';
+
+// const servicesBtn = document.querySelectorAll('.services__item .item__btn'),
+// 	icon = document.querySelector('.item__icon svg path');
+
+// icon.style.fill = '#000';
+// servicesBtn[0].addEventListener('mouseenter', () => {
+// 	console.log(icon);
+// 	icon.style.fill = '#05fab8';
+// });
+// servicesBtn[0].addEventListener('mouseleave', () => {
+// 	console.log(icon);
+// 	icon.style.fill = '#000';
+// });
+// console.log(icon);
