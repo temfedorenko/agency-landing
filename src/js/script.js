5c6ea0ce4a221df13220b41ed5e6985399b02436
Artem <temfedorenko@gmail.com>
2022-10-03 18:42:51 +0300

    burger-menu1

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-// 'use strict';
+'use strict';
 
 // const servicesBtn = document.querySelectorAll('.services__item .item__btn'),
 // 	icon = document.querySelector('.item__icon svg path');
@@ -7,9 +7,39 @@
 // servicesBtn[0].addEventListener('mouseenter', () => {
 // 	console.log(icon);
 // 	icon.style.fill = '#05fab8';
+//    icon.style.transition = 'all 0.6s';
 // });
 // servicesBtn[0].addEventListener('mouseleave', () => {
 // 	console.log(icon);
 // 	icon.style.fill = '#000';
 // });
 // console.log(icon);
+
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+
+   // BURGER MENU
+
+   const burgerMenu = document.querySelector('.burger-menu'),
+         menuContent = document.querySelector('.menu');
+
+   burgerMenu.addEventListener('click', () => {
+
+      if (!menuContent.classList.contains('active')) {
+         menuContent.classList.add('active');
+         burgerMenu.classList.add('burger-menu_active');
+
+      } else {
+         menuContent.classList.remove('active');
+         burgerMenu.classList.remove('burger-menu_active');
+
+      }
+   });
+
+}) 
+
+
+
+
